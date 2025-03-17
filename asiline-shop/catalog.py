5dc9b0dd6253731929d537cec941b6c26b7c4fa9
Nagi <viachistiakov@mail.ru>
2025-03-17 22:36:10 +0300

    Добавленеи каталога

--- asiline-shop/catalog.py
+++ asiline-shop/catalog.py
@@ -1,4 +1,28 @@
 import asyncio
+
+
+@dp.message(Command("enableposting") & F.from_user.id.in_(admin_ids))
+async def enable_posting(message: types.Message):
+    chat_data = dp.chat_data.setdefault(message.chat.id, {})
+    chat_data['posting_mode'] = True
+    await message.reply("Режим постинга включен!")
+
+#Пример команды для выключения режима posting (добавьте, если еще нет)
+@dp.message(Command("disableposting") & F.from_user.id.in_(admin_ids))
+async def disable_posting(message: types.Message):
+    chat_data = dp.chat_data.setdefault(message.chat.id, {})
+    chat_data['posting_mode'] = False
+    await message.reply("Режим постинга выключен!")
+
+
+# Пример обработчика для загрузки изображений (добавьте, если еще нет)
+@dp.message(F.photo & F.from_user.id.in_(admin_ids))
+async def handle_image_upload(message: types.Message):
+    chat_data = dp.chat_data.setdefault(message.chat.id, {})
+
+    file_id = message.photo[-1].file_id  # Берем самое большое разрешение фото
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import KeyboardButton, FSInputFile, ReplyKeyboardMarkup
